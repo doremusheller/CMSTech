09d39f341879e383244c4044be437b849eb671c3
doremusheller <doremusheller@gmail.com>
2026-08-03 21:43:24 -0500

    Install Google Analytics and add privacy disclosure
    * Add website privacy policy
    
    * Install Google Analytics tracking
    
    * Link the website privacy policy

--- site.js
+++ site.js
@@ -1,3 +1,17 @@
+/* Google Analytics 4 */
+(() => {
+  const measurementId = "G-DN7195CR13";
+  window.dataLayer = window.dataLayer || [];
+  window.gtag = window.gtag || function () { window.dataLayer.push(arguments); };
+  window.gtag("js", new Date());
+  window.gtag("config", measurementId);
+
+  const script = document.createElement("script");
+  script.async = true;
+  script.src = `https://www.googletagmanager.com/gtag/js?id=${measurementId}`;
+  document.head.appendChild(script);
+})();
+
 document.addEventListener("DOMContentLoaded", function () {
 
   function initializeNavigation(navRoot) {
